5720ec75659d95b059b6d94d8723daf0f5003a80
elod7 <elod7@lboro.ac.uk>
2015-03-10 17:12:33 +0000

    Complete code for Q4
    Q4 Completed

--- Coursework 2/Question 4/Q4.m
+++ Coursework 2/Question 4/Q4.m
@@ -34,11 +34,7 @@ title('Graph of F(\gamma) for \gamma =-1:1')
 gamma = 0.3421;
 % Calculate L using Im1 equation and R using gamma equation.
 L = ((V0.*sqrt(C).*g(gamma))./Im1).^2
-%Lt = ((tm1./f(gamma))^2).*(1./C)
 R = 2.*gamma.*sqrt(L./C)
-% Gives values of;
-%   L = 36.98 H
-%   R = 3.56*10^-5 Ohm / 35.6 uOhm
 
 
 %% Part D
@@ -46,10 +42,10 @@ R = 2.*gamma.*sqrt(L./C)
 %gamma = 0.5*R*sqrt(C/L)
 omega = (1/sqrt(L*C)).*sqrt(1-(gamma^2))
 t = 0:1*10^-10:0.6*10^-5;
-Im = (V0./(omega.*L)).*sin(omega.*t).*exp(-(R/(2*L)).*t)./(1*10^3);
+Im = (V0./(omega.*L)).*sin(omega.*t).*exp(-(R/(2*L)).*t);
 tm = (sqrt(L.*C)).*((1./sqrt(1-(gamma.^2))).*asin(sqrt(1-(gamma.^2))));
 figure
-plot(t,Im)
+plot(t.*10^6,Im./(1*10^3),'Linewidth',2)
 grid on
 xlabel('Time(\mus)')
 ylabel('I(kA)')
@@ -62,38 +58,50 @@ T = 2*Tz;
 Lest = (T^2)./(4.*(pi^2).*C)
 Rest = (-2./pi).*(sqrt(Lest./C)).*(log(abs(Im2)./Im1))
 
-% %% Part E 
-% % Repeat C and D from the following conditions.
-% C = 54*10^-6;
-% V0 = 27*10^3;
-% Im1 = 1*10^6;
-% tm1 = 1.7*10^-6;
-% % Repeat from part C
-% gamma = -1:0.01:1;
-% F = C.*V0.*f(gamma).*g(gamma)-(tm1*Im1);
-% % Plot
-% figure 
-% plot(gamma,F,'linewidth',2)
-% grid on
-% xlabel('\gamma')
-% ylabel('F(\gamma)')
-% title('Graph of F(\gamma) for \gamma =-1:1')
-% % From datapoint method at F(gamma) = 0, gamma = 0.1464
-% gamma = 0.1464;
-% % Calculate L using Im1 equation and R using gamma equation.
-% L = (V0.*sqrt(C).*g(gamma)).^2
-% R = 2.*gamma.*sqrt(C./L)
-% % Gives values of;
-% %   L = 25.8 kH
-% %   R = 1.3388*10^-5 Ohm / 13.39uOhm
-% % Repeat from Part D
-% omega = (1/sqrt(L*C)).*sqrt(1-(gamma^2))
-% t = 0:0.01:1;
-% Im = (V0./(omega.*L)).*sin(omega.*t).*exp(-(R/(2*L)).*t);
-% tm = (sqrt(L.*C)).*((1./sqrt(1-(gamma.^2))).*asin(sqrt(1-(gamma.^2))));
-% figure
-% plot(t,Im)
-% title('Part E')
-% 
-% %% Part F
-% % Written
+%% Part E 
+% Repeat C and D from the following conditions.
+C2 = 54*10^-6;
+V02 = 27*10^3;
+Im12 = 1*10^6;
+tm12 = 1.7*10^-6;
+% Repeat from part C
+gamma = -1:0.01:1;
+F = C.*V0.*f(gamma).*g(gamma)-(tm1*Im1);
+% Plot
+figure 
+plot(gamma,F,'linewidth',2)
+grid on
+xlabel('\gamma')
+ylabel('F(\gamma)')
+title('Graph of F(\gamma) for \gamma =-1:1')
+% From datapoint method at F(gamma) = 0, gamma = 0.1464
+gamma_c = 0.1464;
+% Calculate L using Im1 equation and R using gamma equation.
+L2 = ((V02.*sqrt(C2).*g(gamma_c))./Im1).^2
+R2 = 2.*gamma_c.*sqrt(L2./C2)
+% Repeat from Part D
+omega = (1/sqrt(L2*C2)).*sqrt(1-(gamma_c^2))
+t2 = 0:1*10^-6:6*10^-3;
+Im_2 = (V02./(omega.*L2)).*sin(omega.*t2).*exp(-(R2/(2*L2)).*t2);
+tm2 = (sqrt(L2.*C2)).*((1./sqrt(1-(gamma_c.^2))).*asin(sqrt(1-(gamma_c.^2))));
+figure
+plot(t2.*10^3,Im_2./(1*10^3),'Linewidth',2)
+grid on
+xlabel('Time(ms)')
+ylabel('I(kA)')
+title('Discharge Current for 27kV V_{0}')
+% Find Im2 and Tz
+% First peak
+Im_2_max = max(Im_2);
+T1idx = find(Im_2 == Im_2_max);
+T1 = t2(T1idx);
+% Second peak
+Im_2_min = min(Im_2);
+Tzidx = find(Im_2 == Im_2_min);
+Tz = t(Tzidx);
+T = 2*Tz;
+Lest2 = (T^2)./(4.*(pi^2).*C2)
+Rest2 = (-2./pi).*(sqrt(Lest./C2)).*(log(abs(Im_2_min)./Im_2_max))
+
+%% Part F
+% Written
