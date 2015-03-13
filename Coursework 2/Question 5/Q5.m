515498ec116c84c87707c205afcdce742ac6f1cf
elod8 <t.p.s.young-11@student.lboro.ac.uk>
2015-03-13 11:08:31 +0000

    Updated Q4
    Added error calculation and correct method to measure gamma from
    F(gamma)

--- Coursework 2/Question 5/Q5.m
+++ Coursework 2/Question 5/Q5.m
@@ -1,97 +1,113 @@
 %% Fast Tansient Sensors - Q5 - Coursework 2
 % B126949 - Tom Young
-
-
 %% Pre - Cursor
 clear all
 clc
-
 %% Constants
-u0 = 4*pi*10^-7; % Free space permeability
-e0 = 8.85*10^-12; % Free space permittivity
 Z0 = 50; % 50 Ohms
-V = 5; % 5V
-Zload1 = 50; % 50 Ohms
-Zload2 = 20; % 20 Ohms
-Zload3 = 75; % 75 Ohms
+V1 = 5; % 5V
+% Setup delta
+delta = [0/4,1/4,2/4,3/4,4/4];
 
 %% Part A
 % i)
-Pload1 = (Zload1-Z0)/(Zload1+Z0); % 0
-V1att2 = V*Pload1; % 0V
+Zl1 = 50;
+Pl1 = (Zl1-Z0)/(Zl1+Z0);
+V1l = V1*Pl1;
+V1l = [0,0,V1l,0,0];
 
 % ii)
-Pload2 = (Zload2-Z0)/(Zload2+Z0); % -0.4286
-V2att2 = V*Pload2; % -  2.14V
+Zl2 = 20;
+Pl2 = (Zl2-Z0)/(Zl2+Z0);
+V2l = V1*Pl2;
+V2l = [0,0,V2l,0,0];
 
 % iii)
-Pload3 = (Zload3-Z0)/(Zload3+Z0); % 0.2
-V3att2 = V*Pload3; % 1V
+Zl3 = 75;
+P3 = (Zl3-Z0)/(Zl3+Z0); % 0.2
+V3l = V1*P3; % 1V
+V3l = [0,0,V3l,0,0];
 
-% Plot Graphs
+% Plot
 figure
-title('Zload = 50 Ohms')
+% i)
 subplot(3,1,1)
-bar(abs(V1att2), 'r')
-title('Zload = 50 Ohms')
+bar(delta,V1l)
+title('Z_{l} = 50\Omega')
 ylabel('Volts (V)')
 xlabel('Time (\delta)')
-%ylim([0, 5])
+ylim([-2.5, 2.5])
+xlim([0,1])
+grid on
+% ii)
 subplot(3,1,2)
-bar(V2att2, 'm')
-title('Zload = 20 Ohms')
+bar(delta,V2l)
+title('Z_{l} = 20\Omega')
 ylabel('Volts (V)')
 xlabel('Time (\delta)')
-%ylim([0, 2.15])
+ylim([-2.5, 2.5])
+xlim([0,1])
+grid on
+% iii)
 subplot(3,1,3)
-bar(abs(V3att2), 'c')
+bar(delta,V3l)
 title('Zload = 75 Ohms')
 ylabel('Volts (V)')
 xlabel('Time (\delta)')
-%ylim([0, 5])
+ylim([-2.5, 2.5])
+xlim([0,1])
+grid on
 
 %% Part B
+V0 = 5;
+delta = [1,2,3,4];
 % i)
-Pload1 = (Zload1-Z0)/(Zload1+Z0); % 0
-V1a = V; % 5V - from 0 -> delta
-V1b = V1a*Pload1; % 0V - from delta -> 2delta
-V1c = V1b*Pload1; % 0V - from 2delta -> 3delta
-V1d = V1c*Pload1; % 0V - from 3delta -> 4delta
-V1 = [abs(V1a), abs(V1b), abs(V1c), abs(V1d)];
+Pl1 = (Zl1-Z0)/(Zl1+Z0);
+V1a = V0; %
+V1b = V1a*Pl1;
+V1c = V1b*Pl1;
+V1d = V1c*Pl1;
+V1 = [V1a, V1b, V1c, V1d];
 
 % ii)
-Pload2 = (Zload2-Z0)/(Zload2+Z0); % -0.4286
-V2a = V; % 5V - from 0 -> delta
-V2b = V2a*Pload2; % 2.14V - from delta -> 2delta
-V2c = V2b*Pload2; % 0.9184V - from 2delta -> 3delta
-V2d = V2c*Pload2; % 0.3936V - from 3delta -> 4delta
-V2 = [abs(V2a), abs(V2b), abs(V2c), abs(V2d)];
+Pl2 = (Zl2-Z0)/(Zl2+Z0);
+V2a = V0;
+V2b = V2a*Pl2;
+V2c = V2b*Pl2;
+V2d = V2c*Pl2;
+V2 = [V2a, V2b, V2c, V2d];
 
 % iii)
-Pload3 = (Zload3-Z0)/(Zload3+Z0); % 0.2
-V3a = V; % 5V - from 0 -> delta
-V3b = V3a*Pload3; % 1V - from delta -> 2delta
-V3c = V3b*Pload3; % 0.2V - from 2delta -> 3delta
-V3d = V3c*Pload3; % 0.04V - from 3delta -> 4delta
-V3 = [abs(V3a), abs(V3b), abs(V3c), abs(V3d)];
+P3 = (Zl3-Z0)/(Zl3+Z0);
+V3a = V0;
+V3b = V3a*P3;
+V3c = V3b*P3;
+V3d = V3c*P3;
+V3 = [V3a, V3b, V3c, V3d];
 
 % Plot graph
 figure
+% i)
 subplot(3,1,1)
-bar(V1, 1, 'g')
-title('Zload = 50 Ohms')
+bar(delta,V1)
+title('Z_{l} = 50\Omega')
 ylabel('Volts (V)')
 xlabel('Time (\delta)')
-ylim([0, 5])
+ylim([-5, 5])
+grid on
+% ii)
 subplot(3,1,2)
-bar(V2, 1, 'b')
-title('Zload = 20 Ohms')
+bar(delta,V2)
+title('Z_{l} = 20\Omega')
 ylabel('Volts (V)')
 xlabel('Time (\delta)')
-ylim([0, 5])
+ylim([-5, 5])
+grid on
+% iii)
 subplot(3,1,3)
-bar(V3, 1, 'y')
-title('Zload = 75 Ohms')
+bar(delta,V3)
+title('Z_{l} = 75\Omega')
 ylabel('Volts (V)')
 xlabel('Time (\delta)')
-ylim([0, 5])
+ylim([-5, 5])
+grid on
